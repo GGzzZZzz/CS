7f9da2b389cf775cbf9dbe42916b3e190a030ccf
StanDimitroff <standimitroff@gmail.com>
2014-03-27 13:55:12 +0200

    C# Part 1 Homeworks

--- Loops-Homework/13.BinaryToDecimalNum/BinaryToDecimalNum.cs
+++ Loops-Homework/13.BinaryToDecimalNum/BinaryToDecimalNum.cs
@@ -5,6 +5,7 @@ class BinaryToDecimalNum
     {
         //Using loops write a program that converts a binary integer number to its decimal form. The input is entered as string. The output should be a variable of type long. Do not use the built-in .NET functionality.
 
+        Console.Write("Enter binary integer number: ");
         string binary = Console.ReadLine();
         byte[] bytes = new byte[binary.Length];//Initialize an array
         for (int i = 0; i < bytes.Length; i++)
---
+++ Loops-Homework/14.DecimalToBinary/DecimalToBinary.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+class DecimalToBinary
+{
+    static void Main()
+    {
+        //Using loops write a program that converts an integer number to its binary representation. The input is entered as long. The output should be a variable of type string. Do not use the built-in .NET functionality.
+
+        long decimalNum = long.Parse(Console.ReadLine());
+        List<long> binList = new List<long>();
+        while (decimalNum > 0)
+        {
+            binList.Add(decimalNum % 2);
+            decimalNum /= 2;
+        }
+        string binary;
+        //for (int i = binList.Count- 1; i >= 0; i--)
+        //{
+            
+        //}
+        binList.Reverse();
+        binary = Convert.ToString(binList);
+
+        Console.WriteLine(binList);
+    }
+}
